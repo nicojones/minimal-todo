2a43e5ae01b7dfecb229ae81e940b5bb4086391f
Nico Kupfer <jones.godel@gmail.com>
2023-01-09 14:53:40 +0545

    changed environment for frontend

--- app/src/services/environment.ts
+++ app/src/services/environment.ts
@@ -1,9 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://192.168.0.2:8080'
+  url: 'https://minimaltodo.com/api'
 };
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  // dev code
-  environment.production = false;
-}
