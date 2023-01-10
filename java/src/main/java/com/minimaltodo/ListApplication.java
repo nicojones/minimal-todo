5434b29f0922a1833fad29af2a7879ced694390c
Nico Kupfer <jones.godel@gmail.com>
2023-01-10 13:37:16 +0545

    Production ready for java+react

--- java/src/main/java/com/minimaltodo/ListApplication.java
+++ java/src/main/java/com/minimaltodo/ListApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(
 	exclude = { SecurityAutoConfiguration.class }
+	// scanBasePackages = {"com.minimaltodo"}
 )
 public class ListApplication {
 
