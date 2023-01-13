7c1c343038228622575467d0b0631b1672b59898
Nico Kupfer <jones.godel@gmail.com>
2023-01-13 11:44:37 +0545

    Dynamic env for React

--- app/src/services/environment.ts
+++ app/src/services/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
-  url: 'http://localhost'
-  // url: 'https://minimaltodo.com'
+  // url: 'http://localhost'
+  url: 'https://minimaltodo.com'
 };
+
+if (process.env.NODE_ENV !== 'production') {
+  environment.url = 'http://localhost'
+}
