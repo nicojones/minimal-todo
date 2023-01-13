967facf6f33d95b0278bc4585939f0da5ee4cad7
Nico Kupfer <jones.godel@gmail.com>
2023-01-13 15:52:09 +0545

    changed server url

--- app/src/services/environment.ts
+++ app/src/services/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // url: 'http://localhost'
-  url: 'https://minimaltodo.com'
+  url: 'https://minimaltodo.com/server'
 };
 
 if (process.env.NODE_ENV !== 'production') {
