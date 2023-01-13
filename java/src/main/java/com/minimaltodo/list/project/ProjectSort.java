1c411ee17dfcd3fcbe7aab96df965a3df5162227
Nico Kupfer <jones.godel@gmail.com>
2023-01-13 10:40:50 +0545

    Remove JAVA

--- java/src/main/java/com/minimaltodo/list/project/ProjectSort.java
+++
@@ -1,9 +0,0 @@
-package com.minimaltodo.list.project;
-
-public enum ProjectSort {
-    A_TO_Z,
-    Z_TO_A,
-    PRIORITY,
-    NEWEST_FIRST,
-    OLDEST_FIRST
-}
--- java/src/main/java/com/minimaltodo/list/user/Role.java
+++
@@ -1,6 +0,0 @@
-package com.minimaltodo.list.user;
-
-public enum Role {
-    USER,
-    ADMIN
-}
