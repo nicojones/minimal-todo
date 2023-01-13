d35f054e1c8209dbb612cfefd18d615f1cf1e61b
Nico Kupfer <jones.godel@gmail.com>
2023-01-13 13:12:28 +0545

    display all errors PHP

--- laravel/public/index.php
+++ laravel/public/index.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 /*
 |--------------------------------------------------------------------------
